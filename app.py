6dfb427d1afbf2f52ba09aa25f8b757639504b82
nix24 <carringtonjaylon4@gmail.com>
2022-03-09 13:24:36 -0500

    implementing feature 2

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from src.repositories.movie_repository import movie_repository_singleton
 
 app = Flask(__name__)
 
+# This repository houses the scaffolding for a movie rating application. By the end, this application will cumplir the following user stories / features:
+
+# As a user, I should be able to view all saved movie ratings in a visually appealing table
+# As a user, I should be able to save a movie rating with the movie's name, directory, and a 1-5 rating
+# As a user, I should be able to search for a specific movie rating using the movie's title
 
 @app.get('/')
 def index():
@@ -12,6 +17,7 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
+    #creating a list of movies
     return render_template('list_all_movies.html', list_movies_active=True)
 
 
@@ -23,6 +29,11 @@ def create_movies_form():
 @app.post('/movies')
 def create_movie():
     # TODO: Feature 2
+    #when user submits form, create a new movie
+    title = request.form.get('title')
+    director = request.form.get('director')
+    rating = request.form.get('rating')
+    movie_repository_singleton.create_movie(title, director, rating)
     # After creating the movie in the database, we redirect to the list all movies page
     return redirect('/movies')
 
@@ -31,3 +42,7 @@ def create_movie():
 def search_movies():
     # TODO: Feature 3
     return render_template('search_movies.html', search_active=True)
+
+# run the application
+if __name__ == "__main__":
+   app.run(debug=True)
