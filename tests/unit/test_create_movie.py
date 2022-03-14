6aaca99747fda465fccc25977802bc1c53c1cb6f
Shail Patel <shail.patel.1116@gmail.com>
2022-03-14 18:05:23 -0400

    Feature 2

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,10 @@
 # TODO: Feature 2
+from src.models.movie import Movie
+
+def test_movie():
+    movie = Movie('Spiderman', 'Sam Raimi', 3)
+
+    assert type(movie) == Movie
+    assert movie.title == 'Spiderman'
+    assert movie.director == 'Sam Raimi'
+    assert movie.rating == 3
