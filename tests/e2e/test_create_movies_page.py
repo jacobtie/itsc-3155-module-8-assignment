5ace2702646f097c68a3d241d7d9035b319c6f1a
nix24 <carringtonjaylon4@gmail.com>
2022-03-14 09:35:07 -0400

    adding test for create_movie function and page
    I was hoping you guys could look over the tests to see if there's any changes I could make

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,24 @@
 # TODO: Feature 2
+# importing app to send requests to client
+#we are testing this function in app.py outside this folder
+# @app.post('/movies')
+# def create_movie():
+#     # TODO: Feature 2
+#     #when user submits form, create a new movie
+#     title = request.form.get('title')
+#     director = request.form.get('director')
+#     rating = request.form.get('rating')
+#     movie_repository_singleton.create_movie(title, director, rating)
+#     # After creating the movie in the database, we redirect to the list all movies page
+#     return redirect('/movies')
+import pytest
+
+
+
+@pytest.fixture(scope='module')
+
+
+def test_create_movie(client):
+    response = client.post('/movies', data=dict(title='Matrix', director='Wachowski', rating='5'))
+    # test to see if the response is a redirect to the list all movies page
+    assert response.location == 'http://localhost/movies'
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,11 @@
 # TODO: Feature 2
+from src.repositories.movie_repository import MovieRepository
+# create test cases
+def test_create_movie():
+    # create a movie
+    movies = MovieRepository()
+    movies.create_movie('Voices of a Distant Star', 'Shinkai', 5)
+    assert len(movies.get_all_movies()) == 1
+    movies.create_movie('Memories', 'Otmomo', 4)
+    movies.create_movie('Matrix', 'Wachowski', 5)
+    assert len(movies.get_all_movies()) == 3
