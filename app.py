e3d1e275741b0eac5905986d253504f6fa3993c3
wDushanin <wendy.dushanin@gmail.com>
2022-03-14 19:02:28 -0400

    it works finally

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, redirect, render_template, request, abort
+from src.models.movie import Movie
 from src.repositories.movie_repository import movie_repository_singleton
 # from src.models.movie import Movie
 
 app = Flask(__name__)
-
-#result = [[]] #empty list in a list to store movie (not working)
-
 
 
 @app.get('/')
@@ -26,29 +24,22 @@ def create_movies_form():
 
 @app.post('/movies')
 def create_movie():
+    title = request.form.get("title", "")
+    director = request.form.get("director", "")
+    rating = request.form.get("rating", "")
+
+    if (
+        "" in (title, director, rating) or
+        not (rating.isdecimal() and 1 <= int(rating) <= 5)
+    ):
+        abort(400)
+    movie_repository_singleton.create_movie(title, director, int(rating))
 
     return redirect('/movies')
 
-@app.get('/movies/search') #Route to the Search Movies form
+
+@app.get('/movies/search')  # Route to the Search Movies form
 def search_movies():
-    #clear previous searches
-    # result.pop[:]
-    return render_template('search_movies.html')
-
-
-@app.get('/result') #route to search resule
-def result():
     input_movie = request.args.get('input_movie', 'Error: No Parameter')
-    movie_repository_singleton.get_movie_by_title(input_movie) #returns a movie object
-    # result.append(searched_movie) #add movie to empty result movie
-
-    #variables for title and rating
-    title = input_movie.title()
-    #rating = input_movie.rating() #keep getting a typecast error, and cannot figure out how to fix
-    
-    return render_template('result.html', title=title, input_movie=input_movie)
-    
-    #rating=rating) #commented out b/c not working due to typecast error
-
-#References for Implement #3: 
-# https://www.geeksforgeeks.org/different-ways-to-clear-a-list-in-python/
+    search_movie = movie_repository_singleton.get_movie_by_title(input_movie)
+    return render_template('search_movies.html', input_movie=input_movie, search_movie=search_movie, search_active=True)
