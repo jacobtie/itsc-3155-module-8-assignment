48e4013fbaa53e512de8782c2d6283badfdc3e16
Jenny1199 <97728718+Jenny1199@users.noreply.github.com>
2022-03-15 01:06:32 -0400

    Update test_get_movie_by_title.py

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,9 @@
 # TODO: Feature 3
+from app import search_movies
+from src.repositories.movie_repository import movie_repository_singleton
+def test_search():
+    movie_repository_singleton.create_movie('Movie', 'Person', 3)
+    movie = movie_repository_singleton.get_movie_by_title("Movie")
+    assert movie.title == "Movie"
+    assert movie.director == "Person"
+    assert movie.rating == 3
