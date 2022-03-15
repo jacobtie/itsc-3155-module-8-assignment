c102cab5b0c2b0fe244e3f70fc2b8eefa553a2d3
Asim Siddique <asim.siddiqm@gmail.com>
2022-03-15 12:36:08 -0400

    Implimented tests

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -7,4 +7,14 @@ def test_search_movies_page(test_app: FlaskClient):
     response = test_app.get('/movies/search')
     response_data = response.data
 
+    assert b'<form action="/movies/display" method="post">' in response_data
+    assert b'<input type="text" class="form-control" id="movie_name" name="movie_name" placeholder="Movie Name">' in response_data
+    assert b'<button type="submit" class="btn btn-primary my-1">Submit</button>' in response_data
+
+def test_display_rating_page(test_app: FlaskClient):
+    response = test_app.get('/movies/display')
+    response_data = response.data
+
+    assert b'<h1 class="mb-5">Movie Ratings</h1>' in response_data
+    assert b'<table class="table">' in response_data
     assert b'<tr><td>{{title}}</td><td>{{rating}}</td></tr>' in response_data
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,9 +1,9 @@
 # TODO: Feature 3
 
 from src.models.movie import Movie
+from src.repositories.movie_repository import MovieRepository
 
 def test_search_movie_model():
-    movie = Movie('Bounty Hunter', 'Khan', 5.9)
-    
-    assert movie.title == 'Star Wars'
-    assert movie.rating == 5
+    movie = Movie('Batman', 'Matt Reeves', 5.9)
+    assert MovieRepository.get_movie_by_title('Batman') == 'Batman'
+    assert MovieRepository.get_movie_by_title('The X Files') == None
