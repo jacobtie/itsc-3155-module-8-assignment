ed8e16f98771575bd0f40a9ee5bb5114295783c2
Xtron5 <rbraswe1@uncc.edu>
2022-03-14 20:22:38 -0400

    Tests added

--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,21 @@
-# TODO: Feature 2
+import pytest
+from src.models.movie import Movie
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_create_movie():
+    testmovie = movie_repository_singleton.create_movie('Sonic Movie', 'Jeff Fowler', 5)
+    emptymovie = movie_repository_singleton.create_movie('','','')
+
+    assert testmovie.title == 'Sonic Movie'
+    assert testmovie.director == 'Jeff Fowler'
+    assert testmovie.rating == 5
+
+def test_empty_movie():
+    emptymovie = movie_repository_singleton.create_movie('','','')
+
+    with pytest.raises(Exception):
+        assert emptymovie.title == 'Sonic Movie' 
+        assert emptymovie.director == 'Jeff Fowler'
+        assert emptymovie.rating == 5
+
+ 
