635592f30e71b8c38003d6ad6c4ee02e89cdf7b8
Jenny1199 <97728718+Jenny1199@users.noreply.github.com>
2022-03-15 01:56:48 -0400

    Update test_search_movies_page.py

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,7 +1,9 @@
 from flask.testing import FlaskClient
 # TODO: Feature 3
 def search_movie_page(test_app: FlaskClient):
+
     response = test_app.get('/movies/search')
     response_data = response.data
     assert  b'<h1 class="mb-5">Search Movie Ratings</h1>' in response_data
     assert response.location == 'http://localhost/movies/search'
+    
