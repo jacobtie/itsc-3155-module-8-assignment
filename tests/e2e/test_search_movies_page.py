a658d0d9dd6e976b40ec03ff9be4890dea133487
wDushanin <wendy.dushanin@gmail.com>
2022-03-14 22:57:18 -0400

    Updating search movie Unit Test

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,6 @@
-# TODO: Feature 3
+from flask.testing import FlaskClient
+
+
+def test_create_movies_page(test_app: FlaskClient):
+    response = test_app.get('/movies/new')
+    response_data = response.data
--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,51 +1,32 @@
-from pytest import fail
-from src.models.movie import Movie
 from src.repositories.movie_repository import MovieRepository
 
 def test_get_all_movies():
     repo = MovieRepository()
    
-    
-
+   #create and add movies to database
     repo.create_movie("Batman", "Caden", 4)
     repo.create_movie("Superman", "William", 5)
     repo.create_movie("Hulk", "Josh", 3)
     repo.create_movie("Star Wars", "George Lucas", 5)
     repo.create_movie("Cats", "Tom Hooper", 1)
 
-  
-
-
-
+    #Test
     assert repo.get_movie_by_title("Batman").title == "Batman"
     assert repo.get_movie_by_title("Superman").title == "Superman"
     assert repo.get_movie_by_title('Hulk').title == "Hulk"
     assert repo.get_movie_by_title('Star Wars').title == "Star Wars"
     assert repo.get_movie_by_title('Cats').title == "Cats"
 
-
-
+    #create variables for test
     movie01 = repo.get_movie_by_title("Batman")
-    movie02 = repo.get_movie_by_title("Superman")
-    movie03 = repo.get_movie_by_title("Hulk")
-    movie04 = repo.get_movie_by_title("Star Wars")
+    movie02 = repo.get_movie_by_title("is not a movie")
+    movie03 = repo.get_movie_by_title("")
+    movie04 = repo.get_movie_by_title("StarWars")
     movie05 = repo.get_movie_by_title("Cats")     
 
-    if movie01 == None:
-        assert False
-    else: 
-        assert True
-
-    
-    if movie01 == None:
-        assert False
-    elif movie02 == None:
-        assert False
-    elif movie03 == None:
-        assert False
-    elif movie04 == None:
-        assert False
-    elif movie05 == None: 
-        assert False
-    else: 
-        assert True
+    #Test
+    assert movie01 is not None
+    assert movie02 is None
+    assert movie03 is None
+    assert movie04 is None
+    assert movie05 is not None
