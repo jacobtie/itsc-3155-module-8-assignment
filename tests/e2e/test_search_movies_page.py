ee09aa4c96240c507b34b4a182e5325fafd8f7aa
Jenny1199 <97728718+Jenny1199@users.noreply.github.com>
2022-03-15 02:29:33 -0400

    Update test_search_movies_page.py

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,7 @@
+from flask.testing import FlaskClient
 # TODO: Feature 3
+def test_search_movie_page(test_app: FlaskClient):
+
+    response = test_app.get('/movies/search')
+    response_data = response.data
+    assert  b'<h1 class="mb-5">Search Movie Ratings</h1>' in response_data
