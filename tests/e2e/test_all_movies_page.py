20e39cf7a1090bf9c0430d4a2dedf87943d3a32c
Qyrep <kevinavila32@gmail.com>
2022-03-15 14:33:58 -0400

    All movies (python, html, and pytests)

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from src.repositories import movie_repository
+from src.models.movie import Movie
+from app import app, create_movie
+from src.repositories.movie_repository import MovieRepository
+
 # TODO: Feature 1
+
+def test_all():
+    client = app.test_client()
+    response = client.get('/movies')
+    all_movies = movie_repository.get_all_movies()
+    if len(all_movies) == 0:
+        assert b"<p>There are no movies</p>"
+    else:
+        assert b"<th>Title</th>" in response.data
+
+
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from src.repositories import movie_repository
+from src.models.movie import Movie
+from app import app, create_movie
+from src.repositories.movie_repository import MovieRepository
+import pytest
+import unittest
+
 # TODO: Feature 1
+
+def test_all():
+    temp = MovieRepository()
+    temp_list = MovieRepository.get_all_movies(self=temp)
+    assert len(temp_list) == 0
+    temp.create_movie(title='Title', director='Director', rating= '5')
+    temp_list = MovieRepository.get_all_movies(self=temp)
+    assert(len(temp_list) == 1)
