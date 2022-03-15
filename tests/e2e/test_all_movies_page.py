dbb3973bff05295bce65d692209396994175fad9
Miles602 <milesmd1426@gmail.com>
2022-03-15 15:29:25 -0400

    Feature-1-Test

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -2,8 +2,8 @@
 from flask.testing import FlaskClient
 
 
-def test_home_page(test_app: FlaskClient):
-    response = test_app.get('/')
+def test_table_page(test_app: FlaskClient):
+    response = test_app.get('/movies')
     response_data = response.data
 
-    assert b'<h1 class="mb-5">Movie Rating App</h1>' in response_data
+    assert b'<h1 class="mb-5">All Movies</h1>' in response_data
