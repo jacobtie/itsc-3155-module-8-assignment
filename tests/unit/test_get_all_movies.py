9b7e966c83abd782108fc3bfec42212c50adfce4
Miles602 <milesmd1426@gmail.com>
2022-03-15 14:13:28 -0400

    Feature-1

--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,31 @@
 # TODO: Feature 1
+
+from src.repositories.movie_repository  import movie_repository_singleton
+
+def test_get_all_movies():
+    title = 'Saving Private Ryan'
+    director = 'Steven Spielberg'
+    rating = int(5)
+    movie_repository_singleton.create_movie(title, director, rating)
+
+
+    for i in movie_repository_singleton.get_all_movies():
+        movieTitle = i.title
+        movieDirector = i.director
+        movieRating = i.rating
+
+    assert movieTitle == title
+    assert movieDirector == director
+    assert movieRating == rating
+
+    movie_2 = movie_repository_singleton._db.pop()
+
+    assert movie_2.title == title
+    assert movie_2.director == director
+    assert movie_2.rating == rating
+
+
+
+    # assert movie[0] == 'Saving Private Ryan'
+    #assert movie[1] == 'Steven Spielburg'
+    #assert movie[2] == 5
