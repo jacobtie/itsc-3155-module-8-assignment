4a33b439180c82de56b5d40547773289b265a490
Jenny1199 <97728718+Jenny1199@users.noreply.github.com>
2022-03-15 02:13:21 -0400

    Update test_get_movie_by_title.py

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -4,7 +4,10 @@ from src.repositories.movie_repository import movie_repository_singleton
 def test_search():
     movie_repository_singleton.create_movie('Movie', 'Person', 3)
     movie = movie_repository_singleton.get_movie_by_title("Movie")
+    movieNot = movie_repository_singleton.get_movie_by_title("Memories")
+    space = movie_repository_singleton.get_movie_by_title("")
     assert movie.title == "Movie"
     assert movie.director == "Person"
     assert movie.rating == 3
-    
+    assert movieNot == None
+    assert space == None
