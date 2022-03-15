114a1d922a4bed355ca019c7db69ac983ee9789d
wDushanin <wendy.dushanin@gmail.com>
2022-03-15 14:17:51 -0400

    Fixing if statment in search movies html

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,6 +1,8 @@
 from flask.testing import FlaskClient
 
 
-def test_create_movies_page(test_app: FlaskClient):
-    response = test_app.get('/movies/new')
+def test_home_page(test_app: FlaskClient):
+    response = test_app.get('/movies/search')
     response_data = response.data
+
+    assert b'<h1 class="mb-5">Movie Rating App</h1>' in response_data
