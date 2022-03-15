4e0e04e5e9d2a7ca78b42c9ea5bc7de8682f347c
Haley Siharath <hsiharat@uncc.edu>
2022-03-15 18:25:43 -0400

    test files

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,20 @@
 # TODO: Feature 1
+from app import app
+from src.repositories.movie_repository import MovieRepository
+
+@pytest.fixture()
+def test_app():
+  return app.test_client()
+
+def test_list_all_movies_page(test_app):
+  response = test_app.get('/movies')
+  assert b'<h1 class="mb-5">All Movies</h1>' in response.data
+
+  assert b'<th scope="col">Ratings</th>' in response.data
+
+def test_list_content_page(test_app):
+    movie_list = MovieRepository()
+    movie_list.create_movie("Hello World", "Hirdhay Tomphson", 5)
+    response = test_app.get('/movies')
+    
+    assert b'<td>Hello World</td>' in response.data
--- tests/unit/test_get_all_movies.py
+++ tests/unit/test_get_all_movies.py
@@ -1,1 +1,13 @@
+from src.repositories.movie_repository import MovieRepository
+
 # TODO: Feature 1
+def test_get_all_movies():
+    movie_list = MovieRepository()
+    assert len(movie_list.get_all_movies) == 0
+
+    movie_list.create_movie("Spider Man", "Haley", 3)
+    assert len(movie_list.get_all_movies) == 1
+
+    movie_list.create_movie("Hello World", "Hirdhay Tomphson", 5)
+    assert len(movie_list.get_all_movies) == 2
+
