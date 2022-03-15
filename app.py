0dd59cfeed46c8cea6da8c6f862bcf69c81bf698
Isaac Rojas Arreola <irojasar@uncc.edu>
2022-03-15 12:21:05 -0400

    Added feature to display table of movie ratings

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template
 from src.repositories.movie_repository import movie_repository_singleton
+from src.models.movie import Movie
 
 app = Flask(__name__)
 
@@ -12,7 +13,8 @@ def index():
 @app.get('/movies')
 def list_all_movies():
     # TODO: Feature 1
-    return render_template('list_all_movies.html', list_movies_active=True)
+    movies = movie_repository_singleton.get_all_movies()
+    return render_template('list_all_movies.html', list_movies_active=True, movies = movies)
 
 
 @app.get('/movies/new')
