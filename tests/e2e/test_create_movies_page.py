4d73feb94b621cc40180603dbed883ad732648a8
Luigiplayer2158 <jps.shs.school@gmail.com>
2022-03-15 16:43:37 -0400

    Adding pytest e2e

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,2 +1,28 @@
 # TODO: Feature 2
-# waiting for all parts to proceed with e2e
+from app import app
+from src.repositories.movie_repository import movie_repository_singleton
+from src.models.movie import Movie
+#https://flask.palletsprojects.com/en/2.0.x/testing/ testing assistance for html / flask form
+
+def test_create_movies_page():
+    test_app = app.test_client()
+
+    title = "Star Wars"
+    director = "George Lucas"
+    rating = "5"
+    # movie info
+
+    test_app.post("/movies", data = 
+        {
+            "title" : title,
+            "director" : director,
+            "rating" : rating
+        })
+    # post the movie info using the create rating page
+
+    page = test_app.get("movies")
+
+    # now we get the movies page and see if the content appears
+    assert b'Star Wars' in page.data
+    assert b'George Lucas' in page.data
+    assert b'5' in page.data
