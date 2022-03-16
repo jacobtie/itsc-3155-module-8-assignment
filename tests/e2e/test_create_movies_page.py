e07b997b03116307af501f75f1c6c9b7f05cac67
popsomebags <aconfidentmeow@gmail.com>
2022-03-15 23:35:58 -0400

    Updated

--- tests/e2e/test_create_movies_page.py
+++ tests/e2e/test_create_movies_page.py
@@ -1,1 +1,11 @@
-# TODO: Feature 2
+from flask.testing import FlaskClient
+from src.repositories.movie_repository import movie_repository_singleton
+from src.models.movie import Movie
+
+def test_create_movies_page(test_app: FlaskClient):
+    response = test_app.post("/movies", title='will', director='star', rating='3')
+    response_data = response.data
+
+    assert response_data.movie.title == 'will'
+    assert response_data.movie.director == 'star'
+    assert response_data.movie.rating == '3'
--- tests/unit/test_create_movie.py
+++ tests/unit/test_create_movie.py
@@ -1,1 +1,10 @@
 # TODO: Feature 2
+from app import app
+from src.repositories.movie_repository import movie_repository_singleton
+
+def test_create_movie():
+    movie = movie_repository_singleton.create_movie("will","william","5")
+
+    assert movie.title == "will"
+    assert movie.director == "william"
+    assert movie.rating == "5"
