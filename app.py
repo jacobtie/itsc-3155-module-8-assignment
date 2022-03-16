855b5874e019d95a655b9cb61c6603ab68377570
Salsada01 <sammybotero1@hotmail.com>
2022-03-15 21:43:13 -0400

    Second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
 from src.repositories.movie_repository import movie_repository_singleton
 
 app = Flask(__name__)
@@ -30,4 +30,9 @@ def create_movie():
 @app.get('/movies/search')
 def search_movies():
     # TODO: Feature 3
-    return render_template('search_movies.html', search_active=True)
+    movie_title =  request.args.get("movieTitle")
+    Movie = movie_repository_singleton.get_movie_by_title(movie_title)
+
+
+   
+    return render_template('search_movies.html', search_active=True, Movie = Movie)
